4610e7718ca5da7d545ae74bddde032c773c9a49
Samuel <92926039+samuel1493@users.noreply.github.com>
2021-12-18 12:52:46 +0100

    Add files via upload

---
+++ Tarea5_ducias2huevos/escripito.js
@@ -0,0 +1,13 @@
+/* 
+	Exemplo ficheiro (a completar) en JavaScript
+	Nome do ficheiro: escripito.js
+	 
+*/
+
+    var nome;
+    nome = prompt("Como te chamas?   ");
+    alert("Guau... que nome máis interesante " + nome);
+    alert("Ti es unha grande persoa " + nome);
+    alert("Vales moito " + nome);
+    alert("Non hai ninguén máis elegante ca " + nome);
+    alert("e... " + nome + ", o mundo adórate!");
