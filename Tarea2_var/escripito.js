0a178c22ccec74964315f808d798710f860fcce8
Samuel <92926039+samuel1493@users.noreply.github.com>
2021-12-19 09:03:09 +0100

    Add files via upload

---
+++ Tarea2_var/escripito.js
@@ -0,0 +1,12 @@
+/* 
+	Exemplo ficheiro (a completar) en JavaScript
+	Nome do ficheiro: escripito.js
+	 
+*/
+    var gastoHoxe;
+    gastoHoxe = 10;
+    alert(gastoHoxe); 
+	var gastoHoxe = 22.22;
+    var gastoOnte = 33.33;
+    var total = gastoHoxe + gastoOnte;
+    alert(total);
