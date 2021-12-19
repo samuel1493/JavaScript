66bdc8046c8ca5c59e6ea0df1eaf7319d4ec392b
Samuel <92926039+samuel1493@users.noreply.github.com>
2021-12-19 09:07:55 +0100

    Add files via upload

---
+++ Tarea4_cadenas/escripito.js
@@ -0,0 +1,16 @@
+/* 
+	Exemplo ficheiro (a completar) en JavaScript
+	Nome do ficheiro: escripito.js
+	 
+*/
+    var euSon = "Profe" + " que " + " conta " + " JavaScript ";
+    alert(euSon);
+	var euSon = "Profe" + " que " + " conta " + "\n" +" JavaScript ";
+    alert(euSon);
+	var oficio = "Profe";
+    var longura = oficio.length;
+    alert(longura);
+	var oficio = "Profe";
+    var rebanada = oficio.slice(3,5);
+    alert(rebanada);
+	
